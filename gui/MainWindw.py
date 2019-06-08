7783de924a3c03d59d90d086b5d42286410007b2
alex97sh-lenovo <alex87sh@yandex.ru>
2019-06-09 01:30:50 +0300

    Добавил несколько элементов на форму.

--- gui/MainWindw.py
+++ gui/MainWindw.py
@@ -7,3 +7,25 @@ class MyWindow(QMainWindow):
         super().__init__()
         self.resize(640, 480)
         self.setWindowTitle("Main Window")
+        self.createUI()
+
+    def createUI(self):
+        self.setCentralWidget(QWidget())
+        lay = QVBoxLayout()
+        self.centralWidget().setLayout(lay)
+        
+        lay_path = QHBoxLayout()
+        lay.addLayout(lay_path)
+        self.m_txtPath = QLineEdit()
+        lay_path.addWidget(self.m_txtPath)
+        self.m_pbOpen = QPushButton("Open")
+        lay_path.addWidget(self.m_pbOpen)
+        
+        lay_grid = QGridLayout()
+        lay.addLayout(lay_grid)
+        self.m_treeLayer = QTreeWidget()
+        lay_grid.addWidget(self.m_treeLayer, 0,0)
+        lay_grid.setColumnStretch(0,20)
+        lay_grid.setColumnStretch(1,80)
+        
+        #lay.addStretch()
