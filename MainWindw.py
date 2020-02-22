c66f5cc73bb53a19a100023cc7d4d9bbd741c092
alex13sh <alex97sh@yandex.ru>
2020-02-22 19:06:09 +0300

    Полностью сделал QmlNode и избавился от generate.

--- MainWindw.py
+++ MainWindw.py
@@ -1,5 +1,5 @@
 from gui.MainWindw import MyWindow as MyMainWindw
-from core import generator as gen
+from core.QmlNode import QmlNode
 import os
 
 class MyWindow(MyMainWindw):
@@ -9,45 +9,8 @@ class MyWindow(MyMainWindw):
         self.setWindowTitle("My Main Window")
         
         def gen_file():
-            txt_node = process_node(self.SelectedLayerNode, "./test/")
-            with open("./test.qml", "w") as f:
-                f.write(txt_node)
+            qmlnode = QmlNode(node=self.SelectedLayerNode)
+            qmlnode.setPath(path_file="./test/") #("./", "./test/")
+            qmlnode.process_node()
+            qmlnode.createFile()
         self.m_pbPrint.clicked.connect(gen_file)
-        
-
-def process_node(node, path="./"):
-    #gen.gen_qml_bynode(self.SelectedLayerNode, {})
-    #txt = gen.gen_qml_type(node, tree=True)
-    
-    typ = "Item" if "group" in node else "Image"
-    if typ=="Item":
-        txt_childs=""
-        for nod in node["group"]:
-            nod_path = path
-            in_sep_file = False
-            if "opt" in nod:
-                opt = nod["opt"]
-                in_sep_file = opt.get("in_sep_file", False)
-                in_sep_dir = opt.get("in_sep_dir", False)
-                if in_sep_dir:
-                    nod_path+=nod["name"]+"/"
-                    os.mkdir(nod_path) # create dir
-                
-            txt_node = process_node(nod, nod_path)
-            if not in_sep_file: txt_childs += txt_node
-            else:
-                with open(nod_path+nod["name"]+".qml", "w") as f:
-                    f.write(txt_node)
-            
-        txt_childs = txt_childs.replace("\n", "\n"+" "*4)
-        res = gen.gen_qml_item(node, txt_childs=txt_childs)
-        if node.get("opt", {}).get("in_sep_dir", False):
-            with open(nod_path+nod["name"]+"/"+nod["name"]+".qml", "w") as f:
-                    f.write(res)
-    else: 
-        lay = node.get("lay", None)
-        if lay:
-            img = lay.topil()
-            node["source"] = "./"+node["name"]+".png"
-            img.save(path+node["source"])
-        return gen.gen_qml_image(node)
--- core/QmlNode.py
+++ core/QmlNode.py
@@ -1,12 +1,23 @@
-
+class GenerateOpt:
+    in_sep_file = False # else in_cur_file
+    in_sep_dir = False # else in_cur_dir
+    
+    # if in_sep_file and in_sep_dir
+    file_in_sep_dir = True # else in_cur_dir
+    file_hold_prop_pos = False # True - свойства позиции храняться в отдельном файле, иначе они указываются в файле использования.
+    
+    #def is
+    
 class QmlNode:
     path = "./"
-    
-    def __init__(self, name="", node=None ):
+    opt_sep = GenerateOpt()
+    def __init__(self, name="", node=None, parent=None ):
         self._name = name
         if node: self.getfromNode(node)
+        #if type(parent) == QmlNode:
+        self._parent = parent
     
-    def getfromNode(node):
+    def getfromNode(self, node):
         self._name = node["name"]
         self._node = node
         self._id = "id_"+self._name;
@@ -17,7 +28,7 @@ class QmlNode:
         self._type = "Item" if "group" in node else "Image"
         self._childs = []
         for nod in node.get("group", []):
-            self._childs.append(QmlNode(node=nod))
+            self._childs.append(QmlNode(node=nod, parent=self))
             
     @staticmethod
     def fromNode(node):
@@ -25,6 +36,76 @@ class QmlNode:
         nod.getfromNode(node)
         return nod
     
-    def setPath(path_app="./", path_qml="./"):
-        self._path_app = path_app # Путь относительно app.py или абсолютный для создания файлов
+    def setPath(self, path_file="./", path_qml="./"):
+        self._path_file = path_file # Путь относительно app.py или абсолютный для создания файлов
+        self._path_app = path_file 
         self._path_qml = path_qml # Путь относительно созданного qml файла или абсолютный для ссалки на изображения.
+        import os
+        try: os.mkdir(self._path_app)
+        except: print("Error path:", self._path_app)
+        
+    def updatePath(self, opt=None):
+        if opt: self.opt_sep=opt
+        else: opt = self.opt_sep
+        if not self._parent or not opt: return
+        import os
+        
+        if opt.in_sep_dir:
+            self._path_app = self._parent._path_app + self._name + "/"
+        else: self._path_app = self._parent._path_app
+        
+        if opt.in_sep_file and opt.file_in_sep_dir:
+            self._path_file = self._path_app # opt.in_sep_dir
+            self._path_qml = "./"
+        else: 
+            self._path_file = self._parent._path_file
+            self._path_qml = self._parent._path_qml + (self._name + "/" if opt.in_sep_dir else "")
+            
+        try: os.mkdir(self._path_app)
+        except: pass
+            
+    def gen_item(self, tree=False):
+        return txt_item.format(**self.__dict__)
+    def gen_image(self):
+        return txt_image.format(**self.__dict__)
+    
+    def process_node(self):
+        if self._type=="Item":
+            self._txt_childs=""
+            for nod in self._childs:
+                nod.updatePath()
+                nod.process_node()
+                if not self.opt_sep.in_sep_file: 
+                    self._txt_childs += nod._txt_node
+                else:
+                    nod.createFile()
+            self._txt_childs = self._txt_childs.replace("\n", "\n"+" "*4)
+            self._txt_node = self.gen_item()
+        else: 
+            self.createImageFile()
+            self._txt_node = self.gen_image()
+            
+    def createFile(self):
+        with open(self._path_file+self._name+".qml", "w") as f:
+            f.write(self._txt_node)
+    def createImageFile(self):
+        if self._lay:
+            img = self._lay.topil()
+            self._source = self._path_qml+ self._name+".png"
+            img.save(self._path_app+ self._name+".png")
+    
+txt_item = """
+Item{{
+    id: {_id}
+    x: {_x}; y: {_y}
+    width: {_w}; height: {_h}
+{_txt_childs}
+}}"""
+
+txt_image = """
+Image{{
+    id: img_{_name}
+    x: {_x}; y: {_y}
+    width: {_w}; height: {_h}
+    source: "{_source}"
+}}"""
